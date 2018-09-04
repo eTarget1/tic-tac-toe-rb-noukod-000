7788da36d7715af331f0d200d50c0b886a912c39
clifford <clifford@noukod.com>
2018-09-04 13:39:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,6 +53,29 @@ end
 end
 
 
+def turn (board)
+ 
+ puts "Please enter 1-9:"
+  
+num = gets.chomp
+  
+index = input_to_index(num)
+  
+if valid_move?(board, index) == true
+    
+move(board, index)
+   
+display_board(board)
+  
+else
+    
+turn(board)
+  
+end
+
+end
+
+
   def play(board)
   play = 0
   input = gets
