ca33fd3440bfa7a32e90fd23dcc986a192fbf507
clifford <clifford@noukod.com>
2018-09-04 13:38:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,6 +36,22 @@ def position_taken?(array, index)
   end
 end
 
+def valid_move?(board, index)
+
+  def position_taken?(array, ind)
+
+if array[ind] == " " || array[ind] == ""  || array[ind] == nil
+
+return false
+
+else
+
+return true
+
+end
+
+end
+
 
   def play(board)
   play = 0
