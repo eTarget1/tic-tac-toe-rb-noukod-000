7b94f061e9afa738ea16989fc38c222fdc445680
clifford <clifford@noukod.com>
2018-09-04 13:34:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,6 +18,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+
+
 
   def play(board)
   play = 0
