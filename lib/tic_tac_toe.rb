854e8a542586c13c15e9fbba32c1f6527a8b541e
clifford <clifford@noukod.com>
2018-09-04 13:42:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -181,12 +181,38 @@ end
 
 def over?(board)
   if won?(board) || full?(board) || draw?(board)
-    
+
 return true
+
+else
+
+return false
+
+end
+
+end
+
+def winner (board)
+  
+index = []
+  
+index = won?(board)
+  
+if index == false
+    
+return nil
   
 else
     
-return false
+if board[index[0]] == "X"
+      
+return "X"
+    
+else
+      
+return "O"
+    
+end
   
 end
 
