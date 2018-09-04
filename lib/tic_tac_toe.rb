9bd41ce524b6f11f09eeda2290932193dcb505dc
clifford <clifford@noukod.com>
2018-09-04 13:42:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -114,44 +114,44 @@ end
 
 
 def won?(board)
-   
+
 WIN_COMBINATIONS.each {|win_combination|
-    
+
 # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-    
+
 # grab each index from the win_combination that composes a win.
-    
+
 win_index_1 = win_combination[0]
-    
+
 win_index_2 = win_combination[1]
-    
+
 win_index_3 = win_combination[2]
 
-    
+
 
 position_1 = board[win_index_1] # load the value of the board at win_index_1
-    
+
 position_2 = board[win_index_2] # load the value of the board at win_index_2
-    
+
 position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-    
+
 
 if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      
+
 return win_combination # return the win_combination indexes that won.
-    
+
 elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      
+
 return win_combination
-    
+
 end
-  
+
 }
-    
-return  
+
+return
 false
-  
+
 end
 
 
@@ -166,20 +166,31 @@ end
 
 
 def draw?(board)
-  
+
 if !won?(board) && full?(board)
+
+return true
+
+elsif !won?(board) && !full?(board) || !won?(board)
+
+return false
+
+end
+
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
     
 return true
   
-elsif !won?(board) && !full?(board) || !won?(board)
+else
     
 return false
   
 end
 
 end
-
-
 
   def play(board)
   play = 0
