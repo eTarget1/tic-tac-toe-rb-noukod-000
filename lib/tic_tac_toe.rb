8f488182f6aac458e20e819af7aa34a2e7bedda3
clifford <clifford@noukod.com>
2018-09-04 13:35:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,17 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, current_player = "X")
+ 
+ board[index] = current_player
+end
 
+
+def position_taken?(board, location)
+  
+board[location] != " " && board[location] != ""
+
+end
 
 
 
